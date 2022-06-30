944f3feec41feaccda3382e1d0bca102c31b02e7
alirezaabdollahpour <alirezaabdollahpour1380@gmail.com>
2022-06-30 17:28:30 +0430

    client function done

--- client/main.py
+++ client/main.py
@@ -12,12 +12,14 @@ import string
 import logging
 import logging.handlers
 import logging.config
+import asyncio
 
 
 # logging.config.fileConfig('logging.conf')
 # logger = logging.getLogger('root')
 
-# define singup function
+
+
 def singup(username, password):
     # curl -X POST http://localhost:8000/signup/ -H 'Content-Type: application/json' -d '{"username":"jakeshkhan","password":"password"}'
     # singup with above curl command
@@ -77,12 +79,32 @@ def get_chats(token):
     # return response
     return response
 
+# define get_chat_messages function with this curl command :
+# curl -X GET http://localhost:8000/get_messages/ -H 'Content-Type: application/json' -d '{"token":"WJW1QTVZLGBKVMB2M8QQA2H1VCN9SAMLTLQQ23RJWV54I8XSMLZGGYTUJI7LF9X8L4ZSBR62GZHVA5MUO5946", "chat_id": 1}'
+def get_messages(token, chat_id):
+    # define url
+    url = "http://localhost:8000/get_messages/"
+    # define headers
+    headers = {'Content-Type': 'application/json'}
+    # define data
+    data = {'token': token, 'chat_id': chat_id}
+    # send request
+    response = requests.get(url, headers=headers, data=json.dumps(data))
+    # return response
+    return response
 
-response = get_chats("WJW1QTVZLGBKVMB2M8QQA2H1VCN9SAMLTLQQ23RJWV54I8XSMLZGGYTUJI7LF9X8L4ZSBR62GZHVA5MUO5946")
-print(response.text)
-
-
-
-
+# define send_message function with this curl command :
+# curl -X POST http://localhost:8000/send_message/ -H 'Content-Type: application/json' -d '{"token":"WJW1QTVZLGBKVMB2M8QQA2H1VCN9SAMLTLQQ23RJWV54I8XSMLZGGYTUJI7LF9X8L4ZSBR62GZHVA5MUO5946", "chat_id": 1, "text": "salam salar"}'
+def send_message(token, chat_id, text):
+    # define url
+    url = "http://localhost:8000/send_message/"
+    # define headers
+    headers = {'Content-Type': 'application/json'}
+    # define data
+    data = {'token': token, 'chat_id': chat_id, 'text': text}
+    # send request
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    # return response
+    return response
 
 
