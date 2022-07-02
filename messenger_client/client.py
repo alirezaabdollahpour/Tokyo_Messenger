739d576223008d71cd6fe2676628ad3df144f949
mohammadrezababaeimosleh <mohammadrezababaeimosleh@gmail.com>
2022-07-02 06:55:09 -1000

    class client implemented

--- messenger_client/client.py
+++ messenger_client/client.py
@@ -75,3 +75,57 @@ def send_message(token, chat_id, text):
     response = requests.post(url, headers=headers, data=json.dumps(data))
     # return response
     return response
+
+class client:
+    # define constructor
+    def __init__(self, username = "", token = ""):
+        # define username
+        self.username = username
+        # define token
+        self.token = token
+        # define chats
+        self.chats = dict({})
+        
+        self.reciever = ""
+        # define messages
+        self.messages = []
+        # define chat_id
+        self.chat_id = 0
+        # define message_id
+        self.message_id = 0
+        # define message_text
+        self.message_text = ""
+        # define message_time
+        self.message_time = ""
+        # define message_sender
+
+
+    # define above create_chat function in this class
+    def create_chat(self, user2name):
+        # call create_chat function
+        response = create_chat(self.token, user2name)
+        # return response
+        return response
+
+    # define above get_chats function in this class
+    def get_chats(self):
+        # call get_chats function
+        response = get_chats(self.token)
+        # return response
+        return response
+
+    # define above get_chat_messages function in this class
+    def get_messages(self, chat_id):
+        # call get_chat_messages function
+        response = get_messages(self.token, chat_id)
+        # return response
+        return response
+
+    # define above send_message function in this class
+    def send_message(self, chat_id, text):
+        # call send_message function
+        response = send_message(self.token, chat_id, text)
+        # return response
+        return response
+
+        
